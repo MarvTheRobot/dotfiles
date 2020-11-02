ebab3cecd8b49178ef2ad8aa7a722889dafee44a
MarvTheRobot <marv@marvtherobot.com>
2020-11-02 16:57:47 +0000

    Change install.ps1; Fix and tidy up messages

--- install.ps1
+++ install.ps1
@@ -12,18 +12,17 @@ function New-Link {
 	)
 
 	$item = Get-Item -Path $Target -Force -ErrorAction SilentlyContinue
-	if($item.Exists -eq $false){
+	if($item -isnot [object] -or $item.Exists -eq $false){
 		Write-Verbose "Creating symlink for $Source -> $Target"
-		$item = New-Link -Value $source -Path $target -ItemType SymbolicLink
+		$item = New-Item -Value $source -Path $target -ItemType SymbolicLink
 	}
 	elseIf($item.Exists -eq $true -and $item.LinkType -ne 'SymbolicLink'){
 		Write-Error "$Target already exists. Cannot make link"
 	}
 	elseIf($item.Exists -eq $true -and $item.LinkType -eq 'SymbolicLink'){
-		Write-Verbose "$Target already a symlink"	
+		Write-Verbose "$Source already has SymLink in $Target"	
 	}
 
-	Write-Output $item
 }
 
 $repo = $PSCommandPath.Replace('install.ps1','')
@@ -33,23 +32,27 @@ $dotfiles = Resolve-Path '~/.dotfiles'
 ## I want a way of sharing some settings between apps and some specific
 if(Get-Command 'code'){
 	$settingsPath = "~/Library/Application Support/Code"
-	New-Link -Source "./settings/vscode/settings.json" -Target "$settingsPath/User/settings.json" 
-	New-Link -Source "./settings/vscode/keybindings.json" -Target "$settingsPath/User/keybindings.json"
+	New-Link -Source "$dotfiles/settings/vscode/settings.json" -Target "$settingsPath/User/settings.json" 
+	New-Link -Source "$dotfiles/settings/vscode/keybindings.json" -Target "$settingsPath/User/keybindings.json"
 }
 
 if(Get-Command 'code-insiders'){
 	$settingsPath = "~/Library/Application Support/Code - Insiders"
-	New-Link -Source "./settings/vscode/settings-insiders.json" -Target "$settingsPath/User/settings.json"
-	New-Link -Source "./settings/vscode/keybindings.json" -Target "$settingsPath/User/keybindings.json"
+	New-Link -Source "$dotfiles/settings/vscode/settings-insiders.json" -Target "$settingsPath/User/settings.json"
+	New-Link -Source "$dotfiles/settings/vscode/keybindings.json" -Target "$settingsPath/User/keybindings.json"
 }
 
 # Link powershell profile
-New-Link -Source "./pwshrc.ps1" -Target "~/.pwshrc.ps1"
-New-Link -Source "./requirements.psd1" -Target "~/.requirements.psd1"
+New-Link -Source "$dotfiles/pwshrc.ps1" -Target "~/.pwshrc.ps1"
+New-Link -Source "$dotfiles/requirements.psd1" -Target "~/.requirements.psd1"
 
 # Link in .config
-Get-ChildItem -Path './config' | foreach-object {
-	New-Link -Source $_.Fullname -Target "~/.config/$($_.Name)" -Verbose
+Get-ChildItem -Path "$dotfiles/config" | foreach-object {
+	if($_.PSIsContainer){
+		New-Link -Source $_.Fullname -Target "~/.config/" -Verbose
+	}else{
+		New-Link -Source $_.Fullname -Target "~/.config/$($_.Name)" -Verbose
+	}
 }
 
 #Change shell
